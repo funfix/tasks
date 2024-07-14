047b323795824e5a17e44f487bbb8e736e087d95
Alexandru Nedelcu <noreply@alexn.org>
2024-07-14 18:57:49 +0300

    Trampoline

--- tasks-core/src/jvmTest/java/org/funfix/tasks/TrampolineTest.java
+++ tasks-core/src/jvmTest/java/org/funfix/tasks/TrampolineTest.java
@@ -1,5 +1,6 @@
-package org.funfix.tasks.internals;
+package org.funfix.tasks;
 
+import org.funfix.tasks.internals.Trampoline;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
