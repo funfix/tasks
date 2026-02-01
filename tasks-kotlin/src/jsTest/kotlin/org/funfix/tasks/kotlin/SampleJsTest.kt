7f4e1bf1fab3ab3ae2b643e4a42b748949ccfcd0
Alexandru Nedelcu <noreply@alexn.org>
2026-02-01 16:31:45 +0200

    Add sample test to fix build

---
+++ tasks-kotlin/src/jsTest/kotlin/org/funfix/tasks/kotlin/SampleJsTest.kt
@@ -0,0 +1,11 @@
+package org.funfix.tasks.kotlin
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SampleJsTest {
+    @Test
+    fun sampleJsTest() {
+        assertEquals(4, 2 + 2)
+    }
+}
