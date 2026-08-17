bd66935ffafc346578eb9098400b219343aa4440
Alexandru Nedelcu <alexandru@users.noreply.github.com>
2026-08-17 15:05:28 +0300

    Update dependencies (#40)

--- tasks-jvm/build.gradle.kts
+++ tasks-jvm/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     errorprone(libs.errorprone.core)
     errorprone(libs.nullaway)
 
-    testImplementation(platform("org.junit:junit-bom:6.1.1"))
+    testImplementation(platform("org.junit:junit-bom:6.1.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
